9998c486c1aba9a3bea7da3565ab6f71e69a2c8c
JES <pengguin12@gmail.com>
2024-05-08 17:22:15 +0900

    update and test 24.05.08

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -4,6 +4,7 @@ permalink: /:basename.min.js
 # PWA service worker
 ---
 
+/**
 const swconfUrl = '{{ '/assets/js/data/swconf.js' | relative_url }}';
 
 importScripts(swconfUrl);
@@ -98,4 +99,22 @@ self.addEventListener('fetch', (event) => {
       });
     })
   );
+});
+*/
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && "navigate" in client) {
+          client.navigate(client.url);
+        }
+      });
+    });
 });
